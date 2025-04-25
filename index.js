bbe179234fa637d9cdea414a5c7e1587caa39ecf
fiona12-code <fionaomondi77@gmail.com>
2025-04-25 12:31:34 +0300

    weather forecast

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ function showCurrentWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed} km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -54,6 +56,51 @@ function handleSearchInput(event) {
   searchCity(searchInputElement.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "3553b3fea60f2bt4edfefb3ad730a38o";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecastHTML = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
+<div class="weather-forecast-day">
+          <div class="forecast-day">${formatDay(day.time)}</div>
+          <div ><img src="${
+            day.condition.icon_url
+          }" class="forecast-icon" /></div>
+          <div class="forecast-temperature">
+            <div class="temperature"><strong>${Math.round(
+              day.temperature.maximum
+            )}°</strong></div>
+            <div class="temperature">${Math.round(
+              day.temperature.minimum
+            )}°</div>
+          </div>
+        </div>`;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHTML;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchInput);
+
 searchCity("Nairobi");
